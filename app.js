986ce9bfcced36f42c719bac825dfb9f6b53a14d
cesarruv <cesaryjoni@gmail.com>
2022-10-06 23:31:08 -0300

    utilizando fetch para consumir json de objetos

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let gondolas = [
+/*let gondolas = [
     {id : 1, nombre : "Parrilla de 90 cm", precio : 1000, marca : "parrillas zona sur", imagen : "./imagenes/parrilla-completa.jpg",},
     {id : 2, nombre : "Brasero De Mesa Enlozado", precio : 1200, marca : "parrillas zona sur", imagen : "./imagenes/Brasero-De-Mesa-Enlozado-Clasico.jpg",},
     {id : 3, nombre : "Tabla AL Recta (50X30)", precio : 800, marca : "parrillas zona sur", imagen : "./imagenes/Tabla-Recta.jpg",},
@@ -6,8 +6,9 @@ let gondolas = [
     {id : 5, nombre : "fogon tamaño medio", precio : 2350, marca : "parrillas zona sur", imagen : "./imagenes/fogon tamaño medio.jpg",},
     {id : 6, nombre : "Disco c/patas Desmontables", precio : 1890, marca : "parrillas zona sur", imagen : "./imagenes/Disco c patas desmontable.jpg",},
     {id : 7, nombre : "Asador Redondo 120 cm", precio : 3700, marca : "parrillas zona sur", imagen : "./imagenes/Asador Redondo 120 cm.jpg",},
-];
+];*/
 
+// BOTONES DE INGRESOS SECCIONES PAGINAS
 let boton = document.getElementById("tienda");
 boton.addEventListener("click", respuesta);
 
@@ -17,6 +18,7 @@ botones2.addEventListener("click", respuesta2)
 let botones3 = document.getElementById("boton3")
 botones3.addEventListener("click", respuesta2)
 
+// SWEET ALERT
 function respuesta2 () {
     Swal.fire({
         title: 'En construcción',
@@ -27,12 +29,14 @@ function respuesta2 () {
         imageAlt: 'Custom image',
       })
 }
-
+ 
+// RECUPERAR STORAGE Y PASO A LA TIENDA
 function respuesta() {
-    
+
     if (localStorage.getItem("carritoGuardado")) {
 
         carroVacio = JSON.parse(localStorage.getItem("carritoGuardado"));
+
         vistaCarrito();
         
     }
@@ -41,13 +45,19 @@ function respuesta() {
     cambio.innerHTML = "";
 
     insertarCards();
-
 }
 
+// GENERAR CARDS
 const insertarCards = () => {
 
     let contenedor = document.getElementById("container");
-    gondolas.forEach((gondola,indice)=>{
+
+    fetch(`/data.json`)
+    .then((Resp)=> Resp.json())
+    .then((gondolas)=>{
+
+        gondolas.forEach((gondola,indice)=>{
+
         let card=document.createElement("div")
         card.classList.add("card", "col-sm-12","col-lg-3", "m-3")
         card.innerHTML = `<img src="${gondola.imagen}" class="card-img-top" alt="...">
@@ -55,13 +65,16 @@ const insertarCards = () => {
           <h5 class="card-title">${gondola.nombre}</h5>
           <h6 class="card-title">marca: ${gondola.marca}</h6>
           <p class="card-text">$${gondola.precio}</p>
-          <a id="botonAccion" class="btn btn-info" onClick="agregado(${indice})">Comprar</a>
+          <a id="botonAccion" class="btn btnColor" onClick="agregado(${indice})">Comprar</a>
         </div>`
 
         contenedor.appendChild(card);
+        })
     })
 }
 
+
+// AGREGAR ARTICULOS AL CARRITO
 const agregado = (indice) => {
 
     Toastify({
@@ -74,39 +87,49 @@ const agregado = (indice) => {
         stopOnFocus: true, 
         className: "cartelToasty",
     }).showToast();
+    
 
-    let indexArticulos = carroVacio.findIndex((elemento)=>{
+    fetch(`/data.json`)
+    .then((Resp)=> Resp.json())
+    .then((gondolas)=>{
 
-        return elemento.id === gondolas[indice].id
+        let indexArticulos = carroVacio.findIndex((elemento)=>{
+            
+            return elemento.id === gondolas[indice]
 
-    });
+        })
 
-    if (indexArticulos === -1) {
-
-        let meterDentro = gondolas[indice]
+    })
+    
+    if ( indexArticulos === -1) {
+        
+        let meterDentro = gondolas[indice].id
         meterDentro.cantidad = 1;
         carroVacio.push(meterDentro);
         vistaCarrito();
-
+        
     } else {
 
         carroVacio[indexArticulos].cantidad += 1;
         vistaCarrito();
-
+        
     }
-
 }
 
-let total = 0;
+total = 0;
 let carroVacio = [];
 let contenedor2 = document.getElementById("carrito");
 
+// GENERAR CARRITO
 const vistaCarrito = () => {
+
     contenedor2.className = "carritoEstilo";
     contenedor2.innerHTML = "";
+
     if (carroVacio.length > 0) {
+
         carroVacio.forEach((producto, indice) => {
-            total = total + producto.precio * producto.cantidad;
+
             let carritoNuevo = document.createElement("div");
             carritoNuevo.classList.add("p-3", "d-flex", "justify-content-between");
             carritoNuevo.innerHTML=
@@ -116,13 +139,93 @@ const vistaCarrito = () => {
             <div class="productos-caracteristicas"> cantidad : ${producto.cantidad} </div>
             <div class="productos-caracteristicas">precio : $${producto.precio} </div>
             <div class="productos-caracteristicas">subtotal : $ ${producto.precio * producto.cantidad} </div>
+            <div class="productos-caracteristicas">
+            <a  class="btn btnColor m-3" onClick="borrarArticulo(${indice})">eliminar articulo</a>
+            
             `;
-            contenedor2.appendChild(carritoNuevo);
 
+            contenedor2.appendChild(carritoNuevo);
 
             subidaJson();
         });
     }
+
+    // ACTUALIZACION DEL TOTAL
+    actualizarTotal();
+
+    if (carroVacio.length > 0) {
+
+        // MOSTRAR TOTAL
+        let sumaFinal = document.createElement("div");
+        sumaFinal.classList.add("p-3", "d-flex", "justify-content-center");
+        sumaFinal.innerHTML=
+        `<div class="productos-caracteristicas estiloTotal justify-content-center" > Suma total del productos: $${total} </div>`
+        
+        contenedor2.appendChild(sumaFinal);
+
+        // BOTON VACIAR CARRITO
+        let vaciar = document.createElement("div");
+        vaciar.classList.add("p-3", "d-flex", "justify-content-center");
+        vaciar.innerHTML=
+        `<a  class="btn btn-info" onClick="vaciarCarrito()">vaciar carrito</a>`
+    
+        contenedor2.appendChild(vaciar);
+
+        // BOTON FINALIZAR COMPRA
+        let finalizarCompra = document.createElement("div");
+        finalizarCompra.classList.add("p-3", "d-flex", "justify-content-center");
+        finalizarCompra.innerHTML=
+        `<a  class="btn btn-info" onClick="ejecutarCarrito()">finalizar compra!</a>`
+    
+        contenedor2.appendChild(finalizarCompra);
+    }
+}
+
+
+
+const borrarArticulo = (indice) => {
+
+    carroVacio.splice(indice,1);
+
+    vistaCarrito();
+}
+
+function actualizarTotal() {
+
+    let totalFinal = carroVacio.reduce((acc, cv) => {
+        return acc + cv.precio * cv.cantidad;
+    }, 0);
+
+    total = totalFinal;
+}
+
+function vaciarCarrito () {
+
+    carroVacio = [];
+    localStorage.clear(); 
+
+    contenedor2.className = "";
+    contenedor2.innerHTML = "";
+
+}
+
+function ejecutarCarrito() {
+
+    contenedor2.className="";
+    contenedor2.classList.add("finalizacionEstilo", "d-flex","flex-column", "justify-content-between")
+    contenedor2.innerHTML = 
+    `
+    <div class="d-flex justify-content-center">
+        <h1 class="text-center">
+            <br> felicitaciones usuario! completaste tu compra <br>
+            <br> el total de tu compra es de $ ${total}.- <br>
+        </h1>
+    </div>
+    <div class="d-flex justify-content-center">
+        <p class = "text-center">(si volves a agregar alguna cantidad de algún articulo vas a poder volver a visualizar el carrito)</p>
+    </div>
+    `;
+
 }
 
 const subidaJson = () => {
